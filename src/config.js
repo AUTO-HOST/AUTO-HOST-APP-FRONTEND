9ef98d3448cc8bf53394a690995d05ea070cb3c8
ALOJAMIENTO AUTOMÁTICO <24300655@uttt.edu.mx>
2025-08-03 23:18:06 -0600

    Actualizar API_BASE_URL en config.js con la URL del nuevo backend en Render.com

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = 'https://auto-host-backend-golden-release.onrender.com/api'; // <--- ¡Esta es la URL correcta!
+// src/config.js
+const API_BASE_URL = 'https://auto-host-backend-ultimate-success.onrender.com/api'; // <--- ¡Esta es la URL correcta!
 export default API_BASE_URL;
